aaf2f57218273d5d6fd1d0f19b6dad0c094dd206
pranatoli <pr.anatoli@gmail.com>
2023-10-13 15:29:39 +0300

    methods layout created for /api/todos/

--- controllers/todos.controller.js
+++ controllers/todos.controller.js
@@ -1,5 +1,26 @@
 const LoginServices = require('../services/login.services');
 
-class ToDosController { }
+class ToDosController {
+    async getToDos(req, res) {
+
+    }
+
+    async addToDo(req, res) {
+
+    }
+
+    async updateTitleToDo(req, res) {
+
+    }
+
+    async isCompletedToDo(req, res) {
+
+    }
+
+    async deleteToDo(req, res) {
+
+    }
+
+}
 
 module.exports = new ToDosController();
--- routes/todos.routes.js
+++ routes/todos.routes.js
@@ -2,7 +2,16 @@ require('dotenv').config();
 
 let express = require('express');
 const routes = express.Router();
-// require controller
+const ToDosControllers = require('../controllers/todos.controller');
 
+routes.get('/', ToDosControllers.getToDos);
+
+routes.post('/', ToDosControllers.addToDo);
+
+routes.patch('/:id', ToDosControllers.updateTitleToDo);
+
+routes.patch('/:id/isCompleted', ToDosControllers.isCompletedToDo);
+
+routes.delete('/:id', ToDosControllers.deleteToDo);
 
 module.exports = routes;
