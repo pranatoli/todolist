ea12e6022251c4e0468771f68437626f0b92f18b
pranatoli <pr.anatoli@gmail.com>
2023-11-30 12:22:45 +0300

    task-5 start write on mongoose

--- config/db.js
+++ config/db.js
@@ -4,6 +4,8 @@ const url = process.env.MONGODBURL;
 const client = new MongoClient(url);
 const dbName = 'todos';
 
+const mongoose = require('mongoose');
+
 async function getConnect() {
     return MongoClient.connect(url).then(client);
 }
@@ -12,6 +14,8 @@ async function getDb() {
     const db = await client.db(dbName);
     return db;
 }
+
+
 
 module.exports = {
     getConnect,
